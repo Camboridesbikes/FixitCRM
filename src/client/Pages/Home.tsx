b9b09a378ed58ac0b11321d3dad89430a0f2fc8d
Cameron H <hattendorfcameron@gmail.com>
2022-05-23 01:41:20 -0700

    edited home page css

--- src/client/Pages/Home.tsx
+++ src/client/Pages/Home.tsx
@@ -6,7 +6,13 @@ import styles from '../styles/modules/home.module.scss'
 
 
 
-const Home = () : JSX.Element => {
+class Home extends React.Component{
+
+    componentDidMount() {
+        document.title = "Home";
+      }
+    
+    render(){
     return(
         <div style={{display: "flex", flexDirection: "column", alignContent: "center", width: "100%", margin: "auto", textAlign: "center"}}>
             <div className={styles.contentContainer}>
@@ -14,7 +20,7 @@ const Home = () : JSX.Element => {
                 <h1>Portland-Based Mobile Bicycle Mechanic</h1>
                 <a href="https://velotooler.com/mechanic-public/cameron.hattendorf">
                     <button 
-                    className={styles.bookingButton}><p>Book a Service</p></button>
+                    className={styles.bookingButton}><p>Book on Velotooler</p></button>
                 </a>
                 <div style={{marginTop: "1.8em"}}>                    
                     <h3>cameron@fixmyvelo.com</h3>
@@ -23,6 +29,7 @@ const Home = () : JSX.Element => {
             </div>            
         </div>
     )
+    }
 }
 
 export default Home;
